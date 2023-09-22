3148d3a890dd91fee7505a99a2e89a5e3d11fb2e
bimamib <bimaprast1601@gmail.com>
2023-09-22 18:56:02 +0700

    melengkapi kode pada MainActivity.kt

--- app/src/main/java/com/bima/mynoteapps/ui/main/MainActivity.kt
+++ app/src/main/java/com/bima/mynoteapps/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.bima.mynoteapps.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bima.mynoteapps.R
+import com.bima.mynoteapps.ViewModelFactory
 import com.bima.mynoteapps.databinding.ActivityMainBinding
+import com.bima.mynoteapps.ui.insert.NoteAddUpdateActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,10 +22,28 @@ class MainActivity : AppCompatActivity() {
         _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+
         adapter = NoteAdapter()
+        
         binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
         binding?.rvNotes?.setHasFixedSize(true)
         binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
     }
 
     override fun onDestroy() {
