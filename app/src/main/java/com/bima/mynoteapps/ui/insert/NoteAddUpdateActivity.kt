134f431b90708879ea787a1c8cf5796574050834
bimamib <bimaprast1601@gmail.com>
2023-09-22 15:12:15 +0700

    membuat package baru bernama ui, kemudian didalamnya membuat 2 package baru (insert dan main), lalu membuat activity baru

---
+++ app/src/main/java/com/bima/mynoteapps/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.bima.mynoteapps.ui.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bima.mynoteapps.R
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
--- app/src/main/java/com/bima/mynoteapps/ui/main/MainActivity.kt
+++ app/src/main/java/com/bima/mynoteapps/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bima.mynoteapps
+package com.bima.mynoteapps.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bima.mynoteapps.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
