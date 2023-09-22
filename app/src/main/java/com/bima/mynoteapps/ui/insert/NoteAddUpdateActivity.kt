36e9ceec7d3b26ba328f531b021e9ddedb212d04
bimamib <bimaprast1601@gmail.com>
2023-09-22 18:29:07 +0700

    menambahkan resource code

--- app/src/main/java/com/bima/mynoteapps/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/bima/mynoteapps/ui/insert/NoteAddUpdateActivity.kt
@@ -2,11 +2,43 @@ package com.bima.mynoteapps.ui.insert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.bima.mynoteapps.R
+import com.bima.mynoteapps.ViewModelFactory
+import com.bima.mynoteapps.database.Note
+import com.bima.mynoteapps.databinding.ActivityNoteAddUpdateBinding
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
+    private var isEdit = false
+    private var note: Note? = null
+
+    private lateinit var noteAddUpdateViewModel: NoteAddUpdateViewModel
+
+    private var _activityNoteAddUpdateBinding: ActivityNoteAddUpdateBinding? = null
+    private val binding get() = _activityNoteAddUpdateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add_update)
+        _activityNoteAddUpdateBinding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        
+        noteAddUpdateViewModel = obtainViewModel(this@NoteAddUpdateActivity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityNoteAddUpdateBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(NoteAddUpdateViewModel::class.java)
     }
 }
