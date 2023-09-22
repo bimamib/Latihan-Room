9df7d664417a3e0f0fed912017a14c98f3f5c824
bimamib <bimaprast1601@gmail.com>
2023-09-22 15:32:07 +0700

    menambahkan kode pada NoteAddUpdateViewModel

--- app/src/main/java/com/bima/mynoteapps/ui/main/MainViewModel.kt
+++ app/src/main/java/com/bima/mynoteapps/ui/main/MainViewModel.kt
@@ -1,4 +1,13 @@
 package com.bima.mynoteapps.ui.main
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.bima.mynoteapps.database.Note
+import com.bima.mynoteapps.repository.NoteRepository
+
+class MainViewModel(application: Application) : ViewModel() {
+    private val mNoteRepository: NoteRepository = NoteRepository(application)
+
+    fun getAllNotes(): LiveData<List<Note>> = mNoteRepository.getAllNotes()
 }
